d4a9248169fb657ddca75bdeb9c13b5d0beeb13e
Ivan Golubey <ig2108@yahoo.com>
2020-04-03 16:50:09 +0300

    Updates

---
+++ precache-manifest.432a35833183cb98060f49f434ee9af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49404d451a479910a8d0f5c1da7b253a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "453289bb3423fee7fe36",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.83589068.chunk.css"
+  },
+  {
+    "revision": "8f0e721519d177a35b11",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.0197419b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.0197419b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453289bb3423fee7fe36",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.96d23246.chunk.js"
+  },
+  {
+    "revision": "70748c9f693cabeb9b11",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.7344c8d1.js"
+  }
+]);
